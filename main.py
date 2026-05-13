0a10db3cfeef2c74a34514dec91f9a108b5c61b9
suraj.be <suraj.be@media.net>
2026-05-13 20:50:17 +0530

    feat: basics of FastApi

--- main.py
+++ main.py
@@ -1,22 +1,43 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, HttpUrl
+from typing import Set, List
+
+from starlette.middleware import base
 
 class Profile(BaseModel):
   name: str
   email: str
   age: int
 
+class Image(BaseModel):
+  url: HttpUrl
+  name: str
+
 class Product(BaseModel):
   name: str
-  price: float
+  price: int = Field(title="title of the product", 
+  description="description of the product",
+  gt=0, lt=1000000)
   discount: int
   discounted_price: float
+  tags: Set[str] = Field(default=["electronics", "fashion"])
+  image: List[Image]
+
+class Offer(BaseModel):
+  name: str
+  description: str
+  price: float
+  products: List[Product]
 
 class User(BaseModel):
   name: str
   email: str
 
 app = FastAPI()
+
+@app.post('/offer')
+def add_offer(offer: Offer):
+  return { offer }
 
 @app.post('/purchase')
 def purchase(user: User, product: Product):
